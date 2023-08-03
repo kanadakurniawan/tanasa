fd100983b538905e9e082317926cb169ee8a7ac3
kanadakurniawan <kanada.kurniawan@gmail.com>
2023-08-03 23:50:00 +0700

    penyesuain css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,47 +36,42 @@ $subjudul = array("Museum of World Writing, Songdo, 2017","Yeoinaru Ferry Termin
                     <div class="content col-md-8"> 
 						<div id="slides">
 							<!-- Button trigger modal -->
-
 							<div class="row">
 								@foreach ($dataPortfolio as $index => $portfolio)
-								{{-- @php
-								$y = $x-1;
-								@endphp --}}
 								<div class="col-3 p-1">
 									<img class="img-fluid" data-toggle="modal" data-target="#exampleModal{{ ++$index }}" src="frontend/images/portfolio/{{ $portfolio->file_name }}.jpg" />									
 								</div>
 								@endforeach
 							</div>
-							<div class="col-2"></div>
 							
 						</div>
 						<div id="tulisan" style="display : none;">
-							<p class="subjudul-konten">curriculum vitae</p> 
-							<p>Davin Tanasa (1985) graduated from University of Melbourne with Master of Architecture in 2010, thesis work with VEIL (Victorian Eco-Innovation Lab) selected as one of top five representative projects for exhibition and featured on the website. In 2014 worked on the construction of Harbin Music Hall in Harbin by Arata Isozaki + HuQian Partners (Arata Isozaki awarded Pritzker Architecture Prize in 2019).</p> 
-							<p style="margin-top:10px;">In 2015 started own office ‘davin tanasa + associates’. In 2017 awarded 3rd Prize for ‘Yeouinaru Ferry Terminal’ in Seoul, Korea, international jury chaired by Alejandro Zaera-Polo and Ryue Nishizawa; and awarded 4th Prize for ‘National Museum of World Writing’ in Songdo, Korea, international jury chaired by Odile Decq.</p> 
-							<p style="margin-top:30px;" class="subjudul-konten">awards</p>							
-							<table style="width:100%">
-							  <tr>
-								<td class="kelas-td"><p>2017</p></td>
-								<td>
-									<p>Yeouinaru Ferry Terminal, Seoul, Korea  [3rd Prize]</p>
-									<p>National Museum of World Writing, Songdo, Korea  [4th Prize]</p>
-								</td>  
-							  </tr>
-							  <tr>
-								<td class="kelas-td"><p>2016</p></td>
-								<td><p>White Garden’s Arcade, Moscow, Russia  [Shortlist]</p></td>
-							  </tr>
-							  <tr>
-								<td class="kelas-td"><p>2015</p></td>
-								<td>
-									<p>Varna Public Library, Varna, Bulgaria  [Shortlist]</p>								
-								</td>
-							  </tr>	
-							  
-							</table>							
+							<div class="row text-justify">
+								<div class="col-6">
+									<p><strong>Profile</strong></p>
+									<p>Davin Tanasa attained Master of Architecture at University of Melbourne in 2010. Prior to office establishment in 2015, worked at various offices in Melbourne, Singapore and Shanghai. Worked at Arata Isozaki 磯崎新 + HuQian 胡倩 Partners on the construction of Harbin Music Hall in 2014. &nbsp;</p>
+									<p>In 2017 awarded 3rd Prize for &lsquo;Yeouinaru Ferry Terminal&rsquo; in Seoul, jury chaired by Alejandro Zaera-Polo and Ryue Nishizawa 西沢立衛; and awarded 4th Prize for &lsquo;National Museum of World Writing&rsquo; in Songdo, jury chaired by Odile Decq. &nbsp;</p>
+									<p><strong>Associates</strong></p>
+									<p>Lu Sheng / 卢生, Associate Partner</p>			
+
+								</div>
+								<div class="col-6">
+									<div class="row">
+										@foreach ($dataAward as $index => $award)
+										<div class="col-6 p-1">
+											<img class="img-fluid" data-toggle="modal" data-target="#exampleModal{{ ++$index }}" src="frontend/images/award/{{ $award->file_name }}.jpg" />
+											<div class="text-center p-2"><p>{{ $award->title }}</p></div>
+											<div class="text-center p-2"><p>{{ $award->subtitle }}</p></div>									
+										</div>
+										@endforeach
+
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
+					
+					<div class="col-2"></div>
 					<div class="sidebar col-md-3">
                         <div class="pinOnScroll">
 
