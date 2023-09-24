ea59f67cffd9f3e1b60787a7df6528ee979226e7
kanadakurniawan <kanada.kurniawan@gmail.com>
2023-09-24 15:29:03 +0700

    tes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,10 +6,10 @@
             </div>
             <div  class="content col-md-8 mb-0">
                 <div class="alamat text-center">
-                    <div class="lh-0">
+                    <div class="lh-0 alamat-baris1">
                         <p>Jl Suprapto IV No 38, Pontianak 78122, KalBar, Indonesia | Contact email: office(at)dvntns.com</p>
                     </div>
-                    <div class="mb-1 pt-2">
+                    <div class="mb-1 pt-2 alamat-baris2">
                         <p>© Davin Tanasa & Associates.</p>
                     </div>
                 </div>
